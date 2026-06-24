f3b89748f1b4e8b68ded91bcfc8bc853c8b72806
daichuan <daichuan@fox-ess.com>
2026-06-24 16:39:36 +0800

    fix: 升级 jnlp agent 从 jdk17 到 jdk21，匹配 Jenkins Controller 版本，解决 UnsupportedClassVersionError

--- configs/jenkins-templates/python-pipeline.groovy
+++ configs/jenkins-templates/python-pipeline.groovy
@@ -62,7 +62,7 @@ spec:
     - name: workspace-volume
       mountPath: /home/jenkins/agent
   - name: jnlp
-    image: swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/jenkins/inbound-agent:latest-jdk17
+    image: swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/jenkins/inbound-agent:latest-jdk21
     imagePullPolicy: Always
   volumes:
   - name: pip-cache
