c471d6839c5f2cea1dcf2d751c2c8482555fc188
Jay Kim <13043329+jay-kim@user.noreply.gitee.com>
2026-07-16 18:54:49 +0800

    fix: disable auto_deploy by default to prevent auto-execution on create

--- k8s-web/src/views/cicd/PipelineCreate.vue
+++ k8s-web/src/views/cicd/PipelineCreate.vue
@@ -2278,7 +2278,7 @@ export default {
         }
       },
       // 自动部署配置
-      auto_deploy: true,
+      auto_deploy: false,
       target_cluster_id: 0,
       target_namespace: '',
       target_workload_kind: 'Deployment',
